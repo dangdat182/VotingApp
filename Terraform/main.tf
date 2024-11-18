5b5b3c4af3891992b9f3932b31282c44a42a51d8
Hưng Trần Thái <21522128@ms.uit.edu.vn>
2024-11-18 13:45:37 +0700

    Update3

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,16 +1,6 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 5.11"
-    }
-  }
-  required_version = ">= 1.3.0"
-}
-
 provider "aws" {
   region = "us-east-1"
 }
